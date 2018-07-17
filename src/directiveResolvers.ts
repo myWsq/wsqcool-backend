94566b5ddc4bc3cc8ce9e1efd5c11bcdffd183ae
wsq <mywsq961@gmail.com>
2018-07-17 23:26:35 +0800

    接口放爆破

--- src/directiveResolvers.ts
+++ src/directiveResolvers.ts
@@ -1,9 +1,27 @@
 import { ContextParameters } from '../node_modules/graphql-yoga/dist/types';
-import { AuthError, getUserId } from './utils';
+import { AuthError, getUserId, getUserIp, gun, getOnce } from './utils';
 import { User } from '../db/entity';
-
-export const isAuthenticated = async (next, source, args, ctx: ContextParameters) => {
+import { resolve } from 'path';
+export const isAuthenticated = async (next, _, __, ctx: ContextParameters) => {
 	const hasUser = getUserId(ctx) && (await User.findOne(getUserId(ctx)));
 	if (hasUser) return next();
 	else throw new AuthError();
+};
+
+// delta: 对应时间段,以秒为单位 times: 对应时间段可以调用几次
+export const rateLimit = async (next, _, args: { times: number; delta: number }, ctx: ContextParameters) => {
+	const user = gun.get('ipPool').get(getUserIp(ctx));
+	const curTimes = user.get('times');
+	const curTimesData = (await getOnce(curTimes)) || 0;
+	const curDelta = user.get('delta');
+	const curDeltaData = (await getOnce(curDelta)) || 0;
+	const now = Math.floor(new Date().getTime() / 1000);
+	if (now - curDeltaData > args.delta) {
+		curDelta.put(now);
+		curTimes.put(1);
+	} else {
+		if (curTimesData > args.times) throw Error('接口超过调用次数限制');
+		else curTimes.put(curTimesData + 1);
+	}
+	return next();
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,11 @@
 import * as jwt from 'jsonwebtoken';
 import { ContextParameters } from '../node_modules/graphql-yoga/dist/types';
-import { BaseEntity } from '../node_modules/typeorm';
+import * as Gun from 'gun';
+
+export const gun = Gun({
+	// radisk: false,
+	// localStorage: false
+});
 
 // 分页参数
 export interface Pagenation {
@@ -8,8 +13,7 @@ export interface Pagenation {
 	take?: number;
 }
 
-
-export function getUserId(ctx: ContextParameters) {
+export const getUserId = (ctx: ContextParameters) => {
 	const Authorization = ctx.request.get('Authorization');
 	if (Authorization) {
 		const token = Authorization.replace('Bearer ', '');
@@ -18,10 +22,25 @@ export function getUserId(ctx: ContextParameters) {
 	}
 
 	throw new AuthError();
-}
+};
 
 export class AuthError extends Error {
 	constructor() {
 		super('未经授权的访问');
 	}
 }
+
+export const getUserIp = (ctx: ContextParameters) => {
+	const req = ctx.request;
+	const headers = req.headers['x-forwarded-for'] as string;
+	return headers ? headers.split(',').pop() : req.connection.remoteAddress || req.socket.remoteAddress || '127.0.0.1';
+};
+
+// 转化为promise
+export const getOnce = (gunIns: any) => {
+	return new Promise<any>((resolve, reject) => {
+		gunIns.once((data) => {
+			resolve(data);
+		});
+	});
+};
